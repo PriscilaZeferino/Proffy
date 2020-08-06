c6cabc0eb14170a83666137fa9df571f9b87cdbc
Priscila Zeferino <priscila.zeferino23@gmail.com>
2020-08-06 16:58:11 -0300

    Update the PageHeader

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
+import PageHeader from '../../components/PageHeader';
 
 import './styles.css'
-
 function TeacherList() {
     return (
 
         <div id="page-teacher-list" className="container">
-
+            <PageHeader title="Estes são os proffys disponiveis."/>
         </div>
     )
 }
