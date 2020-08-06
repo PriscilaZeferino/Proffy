b79dcc792e52075d9b13ca834a02431650213647
Priscila Zeferino <priscila.zeferino23@gmail.com>
2020-08-06 16:56:53 -0300

    Update in the PageHeader and add PageHeaderProps

---
+++ web/src/pages/TeacherList/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './styles.css'
+
+function TeacherList() {
+    return (
+
+        <div id="page-teacher-list" className="container">
+
+        </div>
+    )
+}
+ 
+export default TeacherList;
